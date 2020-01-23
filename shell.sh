13565bdeb202be7399e6cadd7fb8d06d1d9ff727
mineshmane <mineshmane94@gmail.com>
2020-01-23 11:58:44 +0530

    Added:UC2 addtion and multiplication ,Refactor : validations done

--- shell.sh
+++ shell.sh
@@ -4,3 +4,11 @@ echo "Welcome to Arthmetic Computation "
 read -p "Enter value of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
+
+[ -z "${a//[0-9]}" ] && [ -n "$a" ] || echo "Sorry integers only"
+[ -z "${b//[0-9]}" ] && [ -n "$b" ] || echo "Sorry integers only"
+[ -z "${c//[0-9]}" ] && [ -n "$c" ] || echo "Sorry integers only"
+
+
+	addition=$(( $a+$b*$c ))
+	echo $addition
