274db6e8a20169db9e5b085d04451bf1bc68d866
mineshmane <mineshmane94@gmail.com>
2020-01-24 17:13:52 +0530

    Added:UC6_Store result of computaion

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Arthmetic Computation "
 
 read -p "Enter value of a: " a
@@ -8,13 +8,17 @@ read -p "Enter value of c: " c
 [ -z "${a//[0-9]}" ] && [ -n "$a" ] || echo "Sorry integers only"
 [ -z "${b//[0-9]}" ] && [ -n "$b" ] || echo "Sorry integers only"
 [ -z "${c//[0-9]}" ] && [ -n "$c" ] || echo "Sorry integers only"
-
-
+i=0
 	addition=$(( $a+$b*$c ))
+	data[i++]=$addition
 	echo $addition
 	multiplication=$(( $a*$b+$c ))
 	echo $multiplication
+	data[i++]=$multiplication
 	division=$(( ($c + $a) / $b ))
 	echo $division
+	data[i++]=$division
 	modulus=$(( $a % $b + $c ))
 	echo   $modulus 
+	data[i++]=$modulus
+	echo "${data[@]}"
